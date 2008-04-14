9dfdd3e3280d413d6f687d78786f7b8f98a53b13
Nikita V. Youshchenko <yoush@debian.org>
2008-04-15 00:03:22 +0400

    Imported Debian patch 4.2.3.nf2-1

--- debian/cpp-4.2-doc.doc-base.cpp
+++ debian/cpp-4.2-doc.doc-base.cpp
@@ -5,7 +5,7 @@ Abstract: The C preprocessor is a "macro processor" that is used automatically
  by the C compiler to transform your program before actual compilation.
  It is called a macro processor because it allows you to define "macros",
  which are brief abbreviations for longer constructs.
-Section: Apps/Programming
+Section: Programming
 
 Format: html
 Index: /usr/share/doc/cpp-4.2-doc/cpp.html
